ce97086782f8a5cedc51b70152d4b02aa92c2ebf
TVI-ARTEM <azhulin@edu.hse.ru>
2025-05-04 23:38:16 +0300

    Complete

--- src/MyDub.Content/Controllers/FilesController.cs
+++ src/MyDub.Content/Controllers/FilesController.cs
@@ -1,5 +1,6 @@
 ﻿using Amazon.S3;
 using Microsoft.AspNetCore.Mvc;
+using MimeTypes;
 using MyDub.Content.Repositories;
 
 namespace MyDub.Content.Controllers;
@@ -10,26 +11,26 @@ public class FilesController(IS3Repository s3Repository) : ControllerBase
 {
     [HttpPost("upload")]
     [RequestSizeLimit(257_000_000)]
-    public async Task<IActionResult> Upload(IFormFile? file)
+    public async Task<ActionResult<string>> Upload(IFormFile? file)
     {
         if (file is null)
             return BadRequest("File is required");
 
         var key = await s3Repository.UploadFileAsync(file);
-        return Ok(new { key });
+        return Ok(key);
     }
 
     [HttpGet("{key}")]
-    public async Task<IActionResult> Download(string key)
+    public async Task<IResult> Download(string key)
     {
         try
         {
             var response = await s3Repository.GetFileAsync(key);
-            return File(response.ResponseStream, response.Headers.ContentType, key);
+            return Results.File(response.ResponseStream, response.Headers.ContentType, key + MimeTypeMap.GetExtension(response.Headers.ContentType));
         }
         catch (AmazonS3Exception e) when (e.StatusCode == System.Net.HttpStatusCode.NotFound)
         {
-            return NotFound();
+            return Results.NotFound();
         }
     }
 }
